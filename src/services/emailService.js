04527364cd1e8d26756c2d4532e04d68c4a0fac3
Alex Imenwo <aleximenwo@gmail.com>
2025-07-11 08:20:31 +0100

    nodemailer updated

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 // Create Nodemailer transporter
-const transporter = nodemailer.createTransport({
+/* const transporter = nodemailer.createTransport({
   host: 'davidotv.com',
   secure: true,
   port: 465,
@@ -9,9 +9,34 @@ const transporter = nodemailer.createTransport({
     user: 'alex.i@davidotv.com', // your email
     pass: process.env.EMAILPASS, // stored in environment variables
   },
-});
+}); */
 
 // Reusable function to send emails
+/* export const sendEmail = async (email, subject, htmlContent) => {
+  try {
+    await transporter.sendMail({
+      from: 'noreply@davidotv.com', // Sender email
+      to: email,
+      subject: subject,
+      html: htmlContent,
+    });
+    console.log(`Email sent to ${email}`);
+  } catch (error) {
+    console.error(`Error sending email to ${email}: ${error.message}`);
+  }
+}; */
+
+
+const transporter = nodemailer.createTransport({
+  host: 'diamondprojectonline.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: 'alex.i@diamondprojectonline.com', // your email
+    pass: process.env.EMAILPASS, // stored in environment variables
+  },
+});
+
 export const sendEmail = async (email, subject, htmlContent) => {
   try {
     await transporter.sendMail({
@@ -25,3 +50,4 @@ export const sendEmail = async (email, subject, htmlContent) => {
     console.error(`Error sending email to ${email}: ${error.message}`);
   }
 };
+
