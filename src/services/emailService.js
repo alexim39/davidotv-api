de6a2ee94a5290ced62663c7d888263cf7614fb8
Alex Imenwo <aleximenwo@gmail.com>
2025-07-11 08:32:57 +0100

    returned mail service to domain

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 // Create Nodemailer transporter
-/* const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: 'davidotv.com',
   secure: true,
   port: 465,
@@ -9,34 +9,9 @@ import nodemailer from 'nodemailer';
     user: 'alex.i@davidotv.com', // your email
     pass: process.env.EMAILPASS, // stored in environment variables
   },
-}); */
-
-// Reusable function to send emails
-/* export const sendEmail = async (email, subject, htmlContent) => {
-  try {
-    await transporter.sendMail({
-      from: 'noreply@davidotv.com', // Sender email
-      to: email,
-      subject: subject,
-      html: htmlContent,
-    });
-    console.log(`Email sent to ${email}`);
-  } catch (error) {
-    console.error(`Error sending email to ${email}: ${error.message}`);
-  }
-}; */
-
-
-const transporter = nodemailer.createTransport({
-  host: 'diamondprojectonline.com',
-  secure: true,
-  port: 465,
-  auth: {
-    user: 'alex.i@diamondprojectonline.com', // your email
-    pass: process.env.EMAILPASS, // stored in environment variables
-  },
 });
 
+// Reusable function to send emails
 export const sendEmail = async (email, subject, htmlContent) => {
   try {
     await transporter.sendMail({
@@ -51,3 +26,28 @@ export const sendEmail = async (email, subject, htmlContent) => {
   }
 };
 
+
+/* const transporter = nodemailer.createTransport({
+  host: 'diamondprojectonline.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: 'alex.i@diamondprojectonline.com', // your email
+    pass: process.env.EMAILPASS, // stored in environment variables
+  },
+});
+ */
+/* export const sendEmail = async (email, subject, htmlContent) => {
+  try {
+    await transporter.sendMail({
+      from: 'noreply@davidotv.com', // Sender email
+      to: email,
+      subject: subject,
+      html: htmlContent,
+    });
+    console.log(`Email sent to ${email}`);
+  } catch (error) {
+    console.error(`Error sending email to ${email}: ${error.message}`);
+  }
+};
+ */
